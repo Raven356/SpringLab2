bcbce3a4add871331f753540e77d0f89162cb96e
unknown <matroskin7777@gmail.com>
2022-10-22 22:58:03 +0300

    fixed controller

--- src/main/java/com/example/lab2/Lab2Application.java
+++ src/main/java/com/example/lab2/Lab2Application.java
@@ -1,9 +1,12 @@
 package com.example.lab2;
 
+import controller.Controller;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackageClasses = Controller.class)
 public class Lab2Application {
 
     public static void main(String[] args) {
